b59a6298aa1c83c6da0ad58f675c51b9473a2cb4
RyLi13 <103909954+RyLi13@users.noreply.github.com>
2023-01-24 06:31:33 -0500

    Stats screen now lists stats

--- client/src/components/modules/Stats/StatsScreen.js
+++ client/src/components/modules/Stats/StatsScreen.js
@@ -3,10 +3,37 @@ import React from "react";
 import "../../../utilities.css";
 import "./StatsScreen.css";
 
+// const [stats, setStats] = useState({
+//   technical: 0,
+//   presentation: 0,
+//   cooking: 0,
+//   networking: 0,
+//   stat5: 0,
+//   energy: 10,
+//   health: 10,
+//   currentTime: 2,
+//   reputation1: 0,
+//   reputation2: 0,
+//   reputation3: 0,
+//   reputation4: 0,
+// });
+
 const StatsScreen = (props) => {
+  console.log(props.stats);
   return (
     <>
-      <div>Stats here</div>
+      <div>
+        Stats:
+        <ul>
+          {Object.keys(props.stats).map((property) => {
+            return (
+              <li>
+                {property}: {props.stats[property]}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
