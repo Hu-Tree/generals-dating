352754d83949e9340d6d7d2545ef81cb985d6c82
hu-tree <huarthur2@gmail.com>
2023-01-24 04:32:48 -0500

    Weird intermediate thing

---
+++ client/src/components/modules/Dialogue/DialogueBox.js
@@ -0,0 +1,26 @@
+
+//gotta pass some dark magic shit in here like a function???
+
+/**
+ * 
+ * @param {} props 
+ */
+
+
+const OptionsBox = (props) => {
+    
+
+
+}
+
+
+
+const DialogueBox = (props) => {
+
+
+return(
+< div onClick={() => {
+    summon();
+  }}
+
+)}
---
+++ client/src/components/modules/Dialogue/DialogueScreen.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const ChatScreen = () => {
+  return <span> {"PAIN AND SUFFERING "} </span>;
+};
+
+export default ChatScreen;
