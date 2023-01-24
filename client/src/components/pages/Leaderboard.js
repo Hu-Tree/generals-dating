149c4ac2c6a33287ebed2c83bb30a807043acf80
hu-tree <huarthur2@gmail.com>
2023-01-24 13:18:27 -0500

    So true

---
+++ client/src/components/pages/Leaderboard.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { get } from "../../utilities";
+
+import "../../utilities.css";
+import "./Leaderboard.css";
+
+const Leaderboard = (props) => {
+  return <></>;
+};
+
+export default Leaderboard;
