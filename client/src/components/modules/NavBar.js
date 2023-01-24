5aa5a23d6074b25b96ff3b411ada0505ae044f0f
RyLi13 <103909954+RyLi13@users.noreply.github.com>
2023-01-24 03:48:23 -0500

    fix navBar and google authentication

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,51 +1,50 @@
 import React from "react";
 import { Link } from "@reach/router";
-import GoogleLogin, { GoogleLogout } from "react-google-login";
+import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
 
 import "./NavBar.css";
 
 // This identifies your web application to Google's authentication service
-const GOOGLE_CLIENT_ID = "395785444978-7b9v7l0ap2h3308528vu1ddnt3rqftjc.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "718159611952-1cqno013kmj0avh78nki59hfctlgovqt.apps.googleusercontent.com";
 
 /**
  * propTypes
  * @param {string} userId id of current logged in user
+ * @param {function} handleLogin
+ * @param {function} handleLogout
  */
 const NavBar = (props) => {
   return (
-    <nav className="NavBar-container">
-      <div className="NavBar-title u-inlineBlock">Catbook</div>
-      <div className="NavBar-linkContainer u-inlineBlock">
-        <Link to="/" className="NavBar-link">
-          Home
-        </Link>
-        {props.userId && (
-          <Link to={`/profile/${props.userId}`} className="NavBar-link">
-            Profile
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <nav className="NavBar-container">
+        <div className="NavBar-title u-inlineBlock">Catbook</div>
+        <div className="NavBar-linkContainer u-inlineBlock">
+          <Link to="/" className="NavBar-link">
+            Home
           </Link>
-        )}
-        <Link to="/chat/" className="NavBar-link">
-          Chat
-        </Link>
-        {props.userId ? (
-          <GoogleLogout
-            clientId={GOOGLE_CLIENT_ID}
-            buttonText="Logout"
-            onLogoutSuccess={props.handleLogout}
-            onFailure={(err) => console.log(err)}
-            className="NavBar-link NavBar-login"
-          />
-        ) : (
-          <GoogleLogin
-            clientId={GOOGLE_CLIENT_ID}
-            buttonText="Login"
-            onSuccess={props.handleLogin}
-            onFailure={(err) => console.log(err)}
-            className="NavBar-link NavBar-login"
-          />
-        )}
-      </div>
-    </nav>
+          {props.userId && (
+            <Link to={`/profile/${props.userId}`} className="NavBar-link">
+              Profile
+            </Link>
+          )}
+          <Link to="/chat/" className="NavBar-link">
+            Chat
+          </Link>
+          {props.userId ? (
+            <button
+              onClick={() => {
+                googleLogout();
+                props.handleLogout();
+              }}
+            >
+              Logout
+            </button>
+          ) : (
+            <GoogleLogin onSuccess={props.handleLogin} onError={(err) => console.log(err)} />
+          )}
+        </div>
+      </nav>
+    </GoogleOAuthProvider>
   );
 };
 
