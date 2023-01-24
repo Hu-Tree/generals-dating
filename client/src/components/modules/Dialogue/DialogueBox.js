6ed68aee2ac5494f2514f258ed583d8021a1c977
hu-tree <huarthur2@gmail.com>
2023-01-24 04:48:00 -0500

    Dialogue box work

--- client/src/components/modules/Dialogue/DialogueBox.js
+++ client/src/components/modules/Dialogue/DialogueBox.js
@@ -1,16 +1,22 @@
+import react from "react";
+import "./Dialogue.css";
+
 
 //gotta pass some dark magic shit in here like a function???
 
 /**
- * 
- * @param {} props 
+ * Pass in a function, and also the entire option object.
+ * @param {DialogueOption} Option (the entire option object)
+ * @param {Function} Indexer (Passes the destination back)
  */
-
-
 const OptionsBox = (props) => {
     
-
-
+    return(
+    <div className = "optionsBox-container">
+        <div className = "optionsBox-text"> {props.DialogueOption.Text}
+        </div>
+    </div>
+    )
 }
 
 
