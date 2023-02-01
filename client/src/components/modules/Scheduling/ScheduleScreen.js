bb517f0e02be5ddbf12637e8c69fdf4bd81d3b4e
RyLi13 <103909954+RyLi13@users.noreply.github.com>
2023-02-01 05:25:45 -0500

    refactor schedule screen

--- client/src/components/modules/Scheduling/ScheduleScreen.js
+++ client/src/components/modules/Scheduling/ScheduleScreen.js
@@ -29,6 +29,112 @@ const ScheduleScreen = ({ enabled, stats, EVENTS }) => {
     };
   };
 
+  const drawBaseSchedule = () => {
+    return activeScheduleList.map((eventID, index) => {
+      if (getTime().week !== convertTime(index).week) {
+        return <></>;
+      }
+      return (
+        <div
+          className={`card ${EVENTS[eventID].eventDisplay.cssClass} ${
+            index < stats.currentTime ? "shaded" : ""
+          } ${index === stats.currentTime ? "next2" : ""}`}
+          style={{
+            gridColumn: convertTime(index).day,
+            gridRow: convertTime(index).period,
+          }}
+        >
+          {eventID !== "empty" && fixedEventTimes.indexOf(index) === -1 ? (
+            <button
+              className="deleteEventBox"
+              onClick={() => {
+                let newScheduleList = [...activeScheduleList];
+                newScheduleList[index] = "empty";
+                setActiveScheduleList(newScheduleList);
+              }}
+            >
+              ×
+            </button>
+          ) : (
+            <></>
+          )}
+          <h6>{EVENTS[eventID].eventDisplay.name}</h6>
+          <p>{EVENTS[eventID].eventDisplay.description}</p>
+        </div>
+      );
+    });
+  };
+
+  const drawGhostBoxes = () => {
+    return EVENTS[activeOption].eventDisplay.availableTimes.map((allowed, index) => {
+      if (getTime().week !== convertTime(index).week) {
+        return <></>;
+      }
+      if (allowed === "0") {
+        return <></>;
+      }
+      return (
+        <div
+          className={`card ${EVENTS[activeOption].eventDisplay.cssClass} shaded`}
+          style={{
+            gridColumn: convertTime(index).day,
+            gridRow: convertTime(index).period,
+          }}
+          onClick={() => {
+            let newScheduleList = [...activeScheduleList];
+            newScheduleList[index] = activeOption;
+            setActiveScheduleList(newScheduleList);
+          }}
+        >
+          <h6>{EVENTS[activeOption].eventDisplay.name}</h6>
+          <p>{EVENTS[activeOption].eventDisplay.description}</p>
+        </div>
+      );
+    });
+  };
+
+  const eventButton = () => {
+    return (
+      <button
+        className="button"
+        onClick={() => {
+          EVENTS[activeScheduleList[stats.currentTime]].sideEffects();
+        }}
+      >
+        Play Current Event!
+      </button>
+    );
+  };
+
+  const drawOptions = () => {
+    return (
+      <div className="optionsWrapper">
+        {Object.keys(EVENTS).map((eventID) => {
+          if (eventID === "empty" || eventID === "finale") {
+            return <></>;
+          }
+          return (
+            <div
+              onClick={() => {
+                if (activeOption !== eventID) {
+                  setActiveOption(eventID);
+                } else {
+                  setActiveOption("empty");
+                }
+              }}
+              className={`card optionCard
+        ${EVENTS[eventID].eventDisplay.cssClass}
+        ${activeOption === eventID ? "activeOptionCard" : ""}`}
+            >
+              <h6>{EVENTS[eventID].eventDisplay.name}</h6>
+              <p>{EVENTS[eventID].eventDisplay.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   if (!enabled) {
     return <></>;
   }
@@ -40,105 +146,13 @@ const ScheduleScreen = ({ enabled, stats, EVENTS }) => {
         <p>{`Week ${getTime().week} Day ${getTime().day} Period ${getTime().period}`}</p>
         <div className="calendarWrapper">
           <div className="gridWrapper">
-            {activeScheduleList.map((eventID, index) => {
-              if (getTime().week !== convertTime(index).week) {
-                return <></>;
-              }
-              return (
-                <div
-                  className={`card ${EVENTS[eventID].eventDisplay.cssClass} ${
-                    index < stats.currentTime ? "shaded" : ""
-                  } ${index === stats.currentTime ? "next2" : ""}`}
-                  style={{
-                    gridColumn: convertTime(index).day,
-                    gridRow: convertTime(index).period,
-                  }}
-                >
-                  {eventID !== "empty" && fixedEventTimes.indexOf(index) === -1 ? (
-                    <button
-                      className="deleteEventBox"
-                      onClick={() => {
-                        let newScheduleList = [...activeScheduleList];
-                        newScheduleList[index] = "empty";
-                        setActiveScheduleList(newScheduleList);
-                      }}
-                    >
-                      ×
-                    </button>
-                  ) : (
-                    <></>
-                  )}
-                  <h6>{EVENTS[eventID].eventDisplay.name}</h6>
-                  <p>{EVENTS[eventID].eventDisplay.description}</p>
-                </div>
-              );
-            })}
-
-            {activeOption !== "empty" ? (
-              EVENTS[activeOption].eventDisplay.availableTimes.map((allowed, index) => {
-                if (getTime().week !== convertTime(index).week) {
-                  return <></>;
-                }
-                if (allowed === "0") {
-                  return <></>;
-                }
-                return (
-                  <div
-                    className={`card ${EVENTS[activeOption].eventDisplay.cssClass} shaded`}
-                    style={{
-                      gridColumn: convertTime(index).day,
-                      gridRow: convertTime(index).period,
-                    }}
-                    onClick={() => {
-                      let newScheduleList = [...activeScheduleList];
-                      newScheduleList[index] = activeOption;
-                      setActiveScheduleList(newScheduleList);
-                    }}
-                  >
-                    <h6>{EVENTS[activeOption].eventDisplay.name}</h6>
-                    <p>{EVENTS[activeOption].eventDisplay.description}</p>
-                  </div>
-                );
-              })
-            ) : (
-              <></>
-            )}
+            {drawBaseSchedule()}
+            {activeOption !== "empty" ? drawGhostBoxes() : <></>}
           </div>
           <div className="divider"></div>
           <div>
-            <button
-              className="button"
-              onClick={() => {
-                EVENTS[activeScheduleList[stats.currentTime]].sideEffects();
-              }}
-            >
-              Play Current Event!
-            </button>
-
-            <div className="optionsWrapper">
-              {Object.keys(EVENTS).map((eventID) => {
-                if (eventID === "empty" || eventID === "finale") {
-                  return <></>;
-                }
-                return (
-                  <div
-                    onClick={() => {
-                      if (activeOption !== eventID) {
-                        setActiveOption(eventID);
-                      } else {
-                        setActiveOption("empty");
-                      }
-                    }}
-                    className={`card optionCard
-                  ${EVENTS[eventID].eventDisplay.cssClass}
-                  ${activeOption === eventID ? "activeOptionCard" : ""}`}
-                  >
-                    <h6>{EVENTS[eventID].eventDisplay.name}</h6>
-                    <p>{EVENTS[eventID].eventDisplay.description}</p>
-                  </div>
-                );
-              })}
-            </div>
+            {eventButton()}
+            {drawOptions()}
           </div>
         </div>
         {enabled ? <></> : <div className="disableScreen"></div>}
